c2d83f1c5148aa9ebbdb1b01c9036809c45f80d0
Niklas Neronin <nneronin@c2r2p3.hive.fi>
2019-11-22 15:48:43 +0200

    niklas has modified this

--- recursiontest.c
+++ recursiontest.c
@@ -19,6 +19,59 @@
     //if that function returned true
         return (recurstionthing(blocks, map, x + 1, y));
 } */
+int idkvafittanjagbygger(void)
+{
+	int x;
+	int y;
+	char **tmp;
+	int size;
+
+	x = 0;
+	y = 0;
+	size = 4;
+	tmp = ft_strdup(block);
+	while (rec(block, map, x, how_many, y) == 0)
+	{
+			x++
+			block = tmp;
+			if (x == how_many)
+			{
+				x = 0;
+				map = makenewblock(map, size++);
+			}
+	}
+	return (map);
+}
+
+int rec(char **block, char **map, int x, int how_many, int y)
+{
+	if (x > how_many)
+	{
+		x = 0;
+		rec(block, map, x, how_many, y);
+	}
+	if ((jonymagicfunction) == 1 && block[x] != NULL)
+	{
+		y++;
+		block[x] = NULL;
+		if (y == how_many)
+			return (1); //may be return (map)?/?
+		rec(block, map, x++, how_many, y);
+	}
+	if (y > how_many)
+	{
+		if (block[x] != NULL);
+			return (0);
+		return(1); // read previous comment
+	}
+	if (block[x] == NULL)
+		rec(block, map, x++, how_many, y);
+	if ((jonysmagicfunnction) == 0)
+	{
+		y++;
+		rec(block, map, x++, how_many, y);
+	}
+}		
 
 int     iterativething(char ** blocks, char **map, int x, int y, int how_many)
 {
