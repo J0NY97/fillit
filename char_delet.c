dcb4bb73d9d0b7bce8ecc6a6f393f8faa1d9e314
Niklas Neronin <nneronin@c2r2p3.hive.fi>
2019-11-22 12:02:55 +0200

    idk

--- char_delet.c
+++ char_delet.c
@@ -6,9 +6,13 @@
 /*   By: nneronin <nneronin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/22 11:32:55 by nneronin          #+#    #+#             */
-/*   Updated: 2019/11/22 11:49:58 by nneronin         ###   ########.fr       */
+/*   Updated: 2019/11/22 12:02:40 by nneronin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "./libft/libft.h"
 
 void	 map_char_delet(char **map, char c)
 {
@@ -19,12 +23,25 @@ void	 map_char_delet(char **map, char c)
 	y = 0;
 	while(y != 4)
 	{
-		while(x != '\n')
+		while(x != '\0')
 		{
 			if (map[y][x] == c)
-				map[y][x] == '.';
+				map[y][x] = '.';
 			x++;
 		}
 		y++;
 	}
+}
+
+int main(void)
+{
+   char map[4][5] = {
+        {"....\0"},
+        {"..#.\0"},
+        {".##.\0"},
+        {"..#.\0"},
+   };
+	map_char_delet(map, '#');
+	printf("%s\n", map);
+	return (0);
 }
