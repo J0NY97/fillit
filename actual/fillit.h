3df63c60c3182de5708698b9620fcf7b50fef5c8
Jony Salmi <jsalmi@c2r2p1.hive.fi>
2019-11-25 15:48:03 +0200

    rearranged fillit.h

--- actual/fillit.h
+++ actual/fillit.h
@@ -6,7 +6,7 @@
 /*   By: jsalmi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/07 14:23:45 by jsalmi            #+#    #+#             */
-/*   Updated: 2019/11/25 14:24:40 by jsalmi           ###   ########.fr       */
+/*   Updated: 2019/11/25 15:47:38 by jsalmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,32 @@
 # include "./get_next_line.h"
 # include <stdio.h>
 
-char	**makenewblock(char emptychar, int size);
-int		ft_2dstrchr(char **str, char c);
+// Niklas
 int 	pre_rec(int how_many, char **block);
 int		recursive(char **block, char **map, int x, int y, int how_many, int timeout);
-char	**strto2dstr(char *input);
+
+// Jony
 int		actualinsert(char **map, char **block, char emptychar, char filledchar, int howmanieth);
-int		*getfirstinsertable(char **map, int start_x, char filled);
-char	**topleftblock(char **block, int realx, int realy);
 int		*getRealXandY(char **block);
-void	print_block(char **block);
-void	 map_char_delet(char **map, char c);
-int		*get_size(char **block);
+char	**topleftblock(char **block, int realx, int realy);
+int		trytoinsert(char **map, char **block, int startx, char c);
+
+// Arraymaker
+int		part1(char *file);
+char	**part2(char *file, int how_many, char **str);
+
+// Validity checks
+int		validity_check(char *str, char emptychar, char filledchar);
+int		correctchars(char *input, char c1, char c2);
 int		istetriminos(char *input, char c2);
 int		atleast2(char **input, int arr[4][2], char c2);
-int		correctchars(char *input, char c1, char c2);
-char	**part2(char *file, int how_many, char **str);
-int		part1(char *file);
-int		validity_check(char *str, char emptychar, char filledchar);
-int		trytoinsert(char **map, char **block, int startx, char c);
+
+// Help functions
+int		*get_size(char **block);
+void	 map_char_delet(char **map, char c);
+char	**strto2dstr(char *input);
+int		ft_2dstrchr(char **str, char c);
+char	**makenewblock(char emptychar, int size);
+void	print_block(char **block);
 
 #endif
