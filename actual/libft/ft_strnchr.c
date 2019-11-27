089df1d8934edadeb2a1b7d40f8776afde323199
Jony Salmi <jsalmi@c2r2p1.hive.fi>
2019-11-27 16:20:25 +0200

    removed shitty as things from thing

--- actual/libft/ft_strnchr.c
+++ actual/libft/ft_strnchr.c
@@ -6,7 +6,7 @@
 /*   By: jsalmi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 16:13:50 by jsalmi            #+#    #+#             */
-/*   Updated: 2019/11/04 16:14:36 by jsalmi           ###   ########.fr       */
+/*   Updated: 2019/11/27 16:19:36 by jsalmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 char	*ft_strnchr(const char *str, int c, size_t n)
 {
-	size_t i;
-	char *newstr;
+	size_t	i;
+	char	*newstr;
 
 	newstr = (char *)str;
 	i = 0;
