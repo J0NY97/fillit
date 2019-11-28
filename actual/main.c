a3f9fa06ca70fda5bd66b85835d36c88a7b25738
Niklas Neronin <nneronin@c2r2p3.hive.fi>
2019-11-28 12:56:13 +0200

    main

--- actual/main.c
+++ actual/main.c
@@ -6,7 +6,7 @@
 /*   By: jsalmi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/23 12:53:37 by jsalmi            #+#    #+#             */
-/*   Updated: 2019/11/28 11:26:42 by nneronin         ###   ########.fr       */
+/*   Updated: 2019/11/28 12:51:39 by nneronin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,16 @@ int		main(int ac, char **av)
 	int		how_many;
 	int		i;
 
+	how_many = 0;
 	i = 0;
 	str = NULL;
 	if (ac == 2)
 	{
-		how_many = part1(av[1]);
-		if (how_many < 1)
+		str = part0(av[1], how_many, str);
+		if (str == NULL)
 			return (error());
-		str = part2(av[1], how_many, str);
+		while (*str[how_many])
+			how_many++;
 		while (str[i] && i < how_many)
 		{
 			if (!validity_check(str[i], '.', '#'))
